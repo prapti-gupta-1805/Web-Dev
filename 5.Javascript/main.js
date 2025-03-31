91063f5aef88744530f65edd9c8fd06fbb04e93c
Prapti <prapti18056@gmail.com>
2025-03-31 16:18:56 +0530

    js

--- 5.Javascript/main.js
+++ 5.Javascript/main.js
@@ -2,4 +2,13 @@ typeof 'a'
 // will return 'string'
 
 // numbers (data type)
-// positive, negative, integers, decimals
+    // positive, negative, integers, decimals
+    //NaN - not a number (like 0/0)
+
+//declaration of variable
+let a = 1;
+let b;
+
+const pi = 3.14; //cannot be edited/reassigned
+
+var c = 3; //old syntax
