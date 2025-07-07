56304816f84d2780bb8ac8f29d24a496d9244541
Prapti <prapti18056@gmail.com>
2025-07-07 22:32:18 +0530

    js

--- 5.Javascript/dom manipulation/events.js
+++ 5.Javascript/dom manipulation/events.js
@@ -1,19 +1,29 @@
 let btn = document.querySelector(".btn");
-btn.onclick = function() {
-    alert("button was clicked");
-};
+// btn.onclick = function() {
+//     alert("button was clicked");
+// };
 
-//OR
-let btn1 = document.querySelector(".btn1");
-function sayHello() {
+// //OR
+// let btn1 = document.querySelector(".btn1");
+// function sayHello() {
+//     alert("hello!");
+// };
+// btn1.onclick = sayHello;
+
+// //OR
+// let btns = document.querySelectorAll("button");
+// for (let btn of btns) {
+//     btn.onclick = function() {
+//         alert("get wrecked");
+//     };
+// }
+
+//event listeners
+btn.addEventListener("click", function() {
     alert("hello!");
-};
-btn1.onclick = sayHello;
+});
+btn.addEventListener("click", function() {
+    alert("fuck you :)");
+});
 
-//OR
-let btns = document.querySelectorAll("button"); // selects all <button> elements
-for (let btn of btns) {
-    btn.onclick = function() {
-        alert("get wrecked");
-    };
-}
+//similarly 'dblclick' for double click
