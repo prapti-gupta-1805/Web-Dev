21b66fd451afdf104844b450ed072b2060a57a81
Prapti <prapti18056@gmail.com>
2025-07-08 00:26:45 +0530

    js

--- 5.Javascript/dom manipulation/events.js
+++ 5.Javascript/dom manipulation/events.js
@@ -19,6 +19,11 @@ let btn = document.querySelector(".btn");
 // }
 
 //event listeners
+btn.addEventListener("click", function(event) { //callback function takes a default argument event which tells us about event
+    console.log(event);
+});
+
+//mouse events
 btn.addEventListener("click", function() {
     alert("hello!");
 });
@@ -27,3 +32,25 @@ btn.addEventListener("click", function() {
 });
 
 //similarly 'dblclick' for double click
+
+//this in event listeners
+btn.addEventListener("click", function () {
+    console.log(this); //'this' is the object for which event listener is being used
+});
+
+let p = document.querySelector('p');
+
+p.addEventListener("click", function () {
+    console.log(this);
+});
+
+//keyboard events
+//keydown, keyup, keypress, etc.
+
+let text = document.querySelector(".textbox");
+
+text.addEventListener("keydown", function(event) {
+    console.log("key was pressed")
+    console.log(event.key) //returns what key was pressed
+    console.log(event.code) //returns code of key that was pressed
+});
