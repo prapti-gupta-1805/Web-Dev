04f47bc438d3e2b546ed2e65182580a9f315a6a9
Prapti <prapti18056@gmail.com>
2025-07-15 22:38:41 +0530

    js

--- 5.Javascript/runtime-and-networking/runtime.js
+++ 5.Javascript/runtime-and-networking/runtime.js
@@ -18,4 +18,55 @@ changeColor("red", 1000, () => {
     });
 });
 
-//promises
+//promise objects
+
+//code without promises
+function saveToDb(data, success, failure) {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+
+    if (internetSpeed > 4) {
+        success();
+    } else {
+        failure();
+    }
+}
+
+saveToDb("user123", 
+    () => {
+        console.log("Data saved successfully!");
+    },
+    () => {
+        console.log("Failed to save data.");
+    }
+);
+
+//same code with promises
+function saveToDb(data) {
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+
+        if (internetSpeed > 4) {
+            resolve();
+        } else {
+            reject();
+        }
+    });
+}
+
+saveToDb("user123");
+
+//states: pending, fulfilled, rejected
+//resolved -> success callback is called
+//rejected -> failure callback is called, also gives error
+
+//then and catch methods
+saveToDb("user123")
+    .then(() => {
+        console.log("Data saved successfully!");
+    })
+    .catch(() => {
+        console.log("Failed to save data.");
+    });
+
+//then method: runs if promise is resolved
+//catch method: runs if promise is rejected
