38e8cef8c5c27c68ef41cd90508ce1b828b1730e
Prapti <prapti18056@gmail.com>
2025-07-19 00:05:57 +0530

    node.js

--- 3. databases/basics.sql
+++ 3. databases/basics.sql
@@ -22,16 +22,15 @@ SELECT DATABASE();
 -- TABLES
 
 -- Creation
-
 CREATE TABLE teacher (
     id INT PRIMARY KEY,
-    name VARCHAR(100)
+    name VARCHAR(50)
 );
 
 CREATE TABLE students (
     id INT PRIMARY KEY, -- column_name, type, constraint
     name VARCHAR(50),
-    age INT,
+    age INT CHECK (age BETWEEN 18 AND 30),
     t_id INT FOREIGN KEY (t_id) REFERENCES teacher(id)
 );
 
@@ -45,7 +44,6 @@ CREATE TABLE students (
 SHOW TABLES;
 
 -- INSERTION
-
 INSERT INTO teacher (id, name) VALUES
 (1, 'Mr. Anderson'),
 (2, 'Ms. Carter'),
@@ -64,7 +62,6 @@ INSERT INTO students VALUES
 (10, 'Ava Thomas', 19, 1);
 
 -- SELECTION
-
 SELECT * FROM students; -- all rows and columns
 SELECT * FROM students WHERE name IN ('John Doe', 'Jane Smith'); -- multiple values
 SELECT * FROM students WHERE age > 18 AND age < 25; -- AND condition
@@ -74,3 +71,41 @@ SELECT id, name FROM students; -- select specific columns
 SELECT id AS id FROM students; -- aliasing
 SELECT * FROM students LIMIT 5; -- only first 5 records
 SELECT DISTINCT * FROM students; -- remove duplicate records
+
+--aggregate functions
+SELECT AVG(age) FROM students; -- average age
+SELECT MAX(age) FROM students; -- maximum age
+SELECT MIN(age) FROM students; -- minimum age
+SELECT SUM(age) FROM students; -- sum of ages
+SELECT COUNT(*) FROM students; -- count rows
+
+-- group by
+SELECT count(*) AS total_students, age FROM students GROUP BY age; -- count students by age
+SELECT age, COUNT(*) AS total_students FROM students GROUP BY age HAVING COUNT(*) > 1; -- count students by age with condition
+
+--general order
+--SELECT columns FROM table WHERE contition GROUP BY column HAVING condition ORDER BY column ASC/DESC;
+
+-- UPDATION
+UPDATE students SET t_id = 3 WHERE age = 21;
+
+-- DELETION
+DELETE FROM students WHERE age < 20;
+
+-- ALTER
+ALTER TABLE students ADD COLUMN email VARCHAR(100); -- add column
+ALTER TABLE students DROP COLUMN email; -- drop column
+ALTER TABLE students MODIFY COLUMN age INT NOT NULL; -- modify column type or constraint
+ALTER TABLE students CHANGE COLUMN age student_age INT; -- change column name and type
+ALTER TABLE students RENAME COLUMN t_id TO teacher_id; -- rename column
+ALTER TABLE students RENAME TO pupils; -- rename table
+
+-- TRUNCATE
+-- TRUNCATE TABLE pupils; -- remove all records but keep structure
+
+-- JOINS
+SELECT s.id, s.name, t.name AS teacher_name FROM students s INNER JOIN teacher t ON s.teacher_id = t.id; -- Only students with matching teacher
+SELECT s.id, s.name, t.name AS teacher_name FROM students s LEFT JOIN teacher t ON s.teacher_id = t.id; -- All students, matched teachers if any
+SELECT s.id, s.name, t.name AS teacher_name FROM students s RIGHT JOIN teacher t ON s.teacher_id = t.id; -- All teachers, matched students if any
+SELECT s.id, s.name, t.name AS teacher_name FROM students s FULL OUTER JOIN teacher t ON s.teacher_id = t.id; -- All students and all teachers, matched or not
+SELECT s.name, t.name AS teacher_name FROM students s CROSS JOIN teacher t; -- Cartesian product of students and teachers
