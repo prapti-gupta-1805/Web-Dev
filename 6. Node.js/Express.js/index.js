8ec2e5b967c19b099ee5de95e8392f2b03eb3700
Prapti <prapti18056@gmail.com>
2025-07-19 21:06:51 +0530

    expressjs

--- 6. Node.js/Express.js/index.js
+++ 6. Node.js/Express.js/index.js
@@ -8,6 +8,39 @@ app.listen(3000, () => {
 
 //run localhost:3000 in browser to see the server response
 
-app.use((req, res) => {
-    console.log('Request received');
+// //recieving a request
+// app.use((req, res) => {
+//     console.log('Request received');
+//     res.send('<h1>Response sent</h1>'); //sending a response
+// });
+
+//routing
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+// app.get("*", (req, res) => {
+//     res.send('Page Not Found');
+// });
+
+app.post('/', (req, res) => {
+    res.send('Post Request to Home Page');
+});
+
+//use 'nodemon index.js' to automatically restart the server when changes are saved to the file
+
+//path parameters
+// app.get('/:username', (req, res) => {
+//     const username = req.params.username;
+//     res.send(`Welcome, @${username}!`);
+// });
+
+//query strings
+app.get('/search', (req, res) => {
+    const {q} = req.query; //
+    res.send(`Search Results for: ${q}`);
 });
