83e9e09cf4b6f912e87c60d0ea07037bbafd31f3
Prapti <prapti18056@gmail.com>
2025-07-23 20:07:25 +0530

    ejs

--- 6. Node.js/EJS/index.js
+++ 6. Node.js/EJS/index.js
@@ -5,6 +5,7 @@
 //create views folder in the root directory for all templates
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 port = 3000;
@@ -14,8 +15,8 @@ app.listen(port, () => {
 
 //standard line of code
 app.set('view engine', 'ejs'); //set the view engine to EJS
+app.set("views", path.join(__dirname, '/views')); //set the views directory
 
 app.get('/', (req, res) => {
     res.render('home.ejs'); //render the home.ejs file in the views folder
     });
-
